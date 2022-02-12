e26eaa9a6ea105f752126d30b4e237e07f3a36ab
insomnia <talkers-maximsOf@icloud.com>
2022-02-12 19:04:40 +0300

    delete storyboard, create MainTabBarController, FeedViewController, ProfileViewController

---
+++ Navigation/Controllers/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by insomnia on 11.02.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Feed"
+    }
+
+}
---
+++ Navigation/Controllers/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by insomnia on 11.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Profile"
+    }
+
+}
