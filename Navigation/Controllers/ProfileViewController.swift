7afa020ac90fe290a0545e380de16d3f4e80a15d
insomnia <talkers-maximsOf@icloud.com>
2022-02-14 07:30:23 +0300

    add constraints, animation and fixed statusTextField

--- Navigation/Controllers/ProfileViewController.swift
+++ Navigation/Controllers/ProfileViewController.swift
@@ -16,15 +16,17 @@ class ProfileViewController: UIViewController {
         let profileHeaderView = ProfileHeaderView()
         view.addSubview(profileHeaderView)
         setupView()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
     override func viewWillLayoutSubviews() {
         let profileView = ProfileHeaderView()
         profileView.frame = self.view.frame
     }
-    let textField = UITextField(frame: CGRect(x: 195, y: 330, width: 190, height: 50))
-    let showButton = UIButton(frame: CGRect(x: 55, y: 410, width: 300, height: 60))
-    let status = UITextView(frame: CGRect(x: 195, y: 260, width: 190, height: 50))
+    let textField = UITextField(frame: CGRect(x: 200, y: 360, width: 190, height: 50))
+    let showButton = UIButton(frame: CGRect(x: 55, y: 400, width: 300, height: 60))
+    let status = UITextView(frame: CGRect(x: 200, y: 290, width: 190, height: 50))
 
     private func setupView() {
         textField.placeholder = "Waiting with something..."
@@ -38,6 +40,9 @@ class ProfileViewController: UIViewController {
         textField.keyboardType = .default
         textField.clearButtonMode = .always
         view.addSubview(textField)
+        textField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 35).isActive = true
+        textField.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        textField.alpha = 0
 
         showButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         showButton.setTitle("Create Status", for: .normal)
@@ -48,17 +53,27 @@ class ProfileViewController: UIViewController {
         showButton.layer.shadowRadius = 5
         showButton.layer.shadowOpacity = 0.3
         view.addSubview(showButton)
+        showButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        showButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
         
         status.backgroundColor = .systemGray2
         status.font = .systemFont(ofSize: 20)
         status.textColor = .systemGray
         status.text = "status"
         view.addSubview(status)
+        status.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 35).isActive = true
+        status.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
     }
 
     @objc func buttonPressed() {
         status.text = textField.text
         status.textColor = .black
+        UIView.animate(withDuration: 2.0) {
+            self.showButton.center = self.view.center
+            self.textField.alpha = 1
+            self.view.endEditing(true)
+        }
     }
 }
 
--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -24,10 +24,18 @@ class ProfileHeaderView: UIView {
         let profileImageView = UIImageView(image: profileImage)
         profileImageView.frame = CGRect(x: 35, y: 150, width: 150, height: 230)
         self.addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.heightAnchor.constraint(equalToConstant: 230).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 150).isActive = true
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 35).isActive = true
+        profileImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 380).isActive = true
         
-        let name = UILabel(frame: CGRect(x: 215, y: 170, width: 200, height: 80))
+        let name = UILabel(frame: CGRect(x: 215, y: 190, width: 200, height: 80))
         name.text = "K1LL B1LL"
-        name.font = .systemFont(ofSize: 30, weight: .bold)
+        name.font = .systemFont(ofSize: 35, weight: .bold)
         self.addSubview(name)
+        name.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 35).isActive = true
+        name.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
     }   
 }
