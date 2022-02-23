ed3d038fc874c7bb66cd9bab9f221eda3be5984e
insomnia <talkers-maximsOf@icloud.com>
2022-02-23 14:11:03 +0300

    refresh code, add constraint

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -11,7 +11,7 @@ class ProfileHeaderView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
+        addConstraints()
         let tap = UITapGestureRecognizer(target: self, action: #selector(tap(_:)))
         self.addGestureRecognizer(tap)
     }
@@ -20,24 +20,29 @@ class ProfileHeaderView: UIView {
         super.init(coder: coder)
     }
     
-    let textField = UITextField(frame: CGRect(x: 200, y: 290, width: 160, height: 50))
-    let showButton = UIButton(frame: CGRect(x: 200, y: 290, width: 160, height: 50))
-    let status = UITextView(frame: CGRect(x: 200, y: 210, width: 160, height: 50))
-    
-    private func setupView() {
-        
-        let profileImage = UIImage(named: "portrait")
-        let profileImageView = UIImageView(image: profileImage)
-        profileImageView.frame = CGRect(x: 35, y: 120, width: 150, height: 230)
-        self.addSubview(profileImageView)
-
-        let name = UILabel(frame: CGRect(x: 200, y: 120, width: 200, height: 80))
+    private lazy var name: UILabel = {
+        let name = UILabel()
         name.text = "K1LL B1LL"
         name.font = .systemFont(ofSize: 35, weight: .bold)
         name.adjustsFontSizeToFitWidth = true
         name.minimumScaleFactor = 0.5
         self.addSubview(name)
-
+        name.translatesAutoresizingMaskIntoConstraints = false
+        return name
+    }()
+    
+    private lazy var status: UITextView = {
+        let status = UITextView()
+        status.backgroundColor = .systemGray2
+        status.font = .systemFont(ofSize: 20)
+        status.textColor = .systemGray
+        status.text = "status"
+        status.translatesAutoresizingMaskIntoConstraints = false
+        return status
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
         textField.placeholder = "Waiting with something..."
         textField.adjustsFontSizeToFitWidth = true
         textField.minimumFontSize = 0.5
@@ -51,8 +56,12 @@ class ProfileHeaderView: UIView {
         textField.keyboardType = .default
         textField.clearButtonMode = .always
         textField.alpha = 0
-        self.addSubview(textField)
-
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    } ()
+    
+    private lazy var showButton: UIButton = {
+        let showButton = UIButton()
         showButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         showButton.setTitle("Create Status", for: .normal)
         showButton.backgroundColor = .systemBlue
@@ -61,31 +70,79 @@ class ProfileHeaderView: UIView {
         showButton.layer.shadowOffset = CGSize(width: 5, height: 5)
         showButton.layer.shadowRadius = 5
         showButton.layer.shadowOpacity = 0.3
-        self.addSubview(showButton)
+        showButton.translatesAutoresizingMaskIntoConstraints = false
+        return showButton
+    }()
 
-        status.backgroundColor = .systemGray2
-        status.font = .systemFont(ofSize: 20)
-        status.textColor = .systemGray
-        status.text = "status"
-        self.addSubview(status)
-    }
+    let avatarImage: UIImageView = {
+        let portrait = UIImage(named: "portrait")
+        let avatarImage = UIImageView(image: portrait)
+        avatarImage.translatesAutoresizingMaskIntoConstraints = false
+        return avatarImage
+    } ()
 
     @objc func buttonPressed() {
         status.text = textField.text
         status.textColor = .black
         textField.text = ""
         UIView.animate(withDuration: 1.0) {
-            self.showButton.frame = CGRect(x: 200, y: 370, width: 160, height: 50)
+            self.showButton.frame = CGRect(x: 205, y: 320, width: 160, height: 50)
             self.textField.alpha = 1
             self.endEditing(true)
             if self.status.hasText {
                 self.showButton.setTitle("Change Status", for: .normal)
                 self.textField.alpha = 0
-                self.showButton.frame = CGRect(x: 200, y: 290, width: 160, height: 50)
+                self.showButton.frame = CGRect(x: 205, y: 270, width: 160, height: 50)
             }
         }
     }
+    
     @objc func tap(_ sender: Any) {
         textField.resignFirstResponder()
+    }
+    
+    private lazy var labelStackView: UIStackView = {
+        let labelstackView = UIStackView()
+        labelstackView.axis = .vertical
+        labelstackView.distribution = .fillEqually
+        labelstackView.spacing = 20
+        labelstackView.translatesAutoresizingMaskIntoConstraints = false
+        return labelstackView
+    }()
+
+    private func addConstraints() {
+        
+        self.addSubview(labelStackView)
+        self.addSubview(avatarImage)
+        self.addSubview(textField)
+        self.addSubview(showButton)
+        labelStackView.addArrangedSubview(name)
+        labelStackView.addArrangedSubview(status)
+
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(avatarImage.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 10))
+        constraints.append(avatarImage.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: 20))
+        constraints.append(avatarImage.trailingAnchor.constraint(greaterThanOrEqualTo: self.trailingAnchor, constant: -240))
+        constraints.append(avatarImage.bottomAnchor.constraint(lessThanOrEqualTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 400))
+        
+        constraints.append(labelStackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 50))
+        constraints.append(labelStackView.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: 30))
+        constraints.append(labelStackView.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -20))
+        constraints.append(labelStackView.bottomAnchor.constraint(lessThanOrEqualTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 450))
+        
+        constraints.append(textField.topAnchor.constraint(equalTo: labelStackView.bottomAnchor, constant: 20))
+        constraints.append(textField.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: 30))
+        constraints.append(textField.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -30))
+        constraints.append(textField.bottomAnchor.constraint(lessThanOrEqualTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 350))
+        
+        constraints.append(showButton.topAnchor.constraint(equalTo: labelStackView.bottomAnchor, constant: 20))
+        constraints.append(showButton.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: 30))
+        constraints.append(showButton.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -20))
+        constraints.append(showButton.bottomAnchor.constraint(lessThanOrEqualTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 350))
+        constraints.append(showButton.heightAnchor.constraint(equalToConstant: 50))
+        constraints.append(showButton.widthAnchor.constraint(equalToConstant: 160))
+    
+        NSLayoutConstraint.activate(constraints)
     }
 }
