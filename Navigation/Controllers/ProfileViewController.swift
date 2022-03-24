5702504549f2c6358c5924d699aa5304af55c0c3
insomnia <talkers-maximsOf@icloud.com>
2022-03-24 15:05:05 +0300

    clear code, fix tableView

--- Navigation/Controllers/ProfileViewController.swift
+++ Navigation/Controllers/ProfileViewController.swift
@@ -11,6 +11,7 @@ class ProfileViewController: UIViewController {
     
     let profileView = ProfileHeaderView()
     let postView = PostZoomView()
+    let avatarView = AvatarZoomView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,84 +23,24 @@ class ProfileViewController: UIViewController {
         profileView.avatarImage.addGestureRecognizer(avatarTap)
     }
     
-    lazy var avatarView: UIView = {
-        let avatarView = UIView()
-        avatarView.backgroundColor = .white
-        avatarView.alpha = 0
-        avatarView.translatesAutoresizingMaskIntoConstraints = false
-        return avatarView
-    }()
-    
-    private lazy var avatarImage: UIImageView = {
-        let portrait = UIImage(named: "portrait")
-        let avatarImage = UIImageView(image: portrait)
-        avatarImage.layer.shadowColor = UIColor.black.cgColor
-        avatarImage.layer.shadowOffset = CGSize(width: 10, height: 10)
-        avatarImage.layer.shadowRadius = 10
-        avatarImage.layer.shadowOpacity = 0.3
-        avatarImage.translatesAutoresizingMaskIntoConstraints = false
-        return avatarImage
-    } ()
-    
-    private lazy var exitButton: UIButton = {
-        let exitButton = UIButton()
-        exitButton.setTitle("X", for: .normal)
-        exitButton.addTarget(self, action: #selector(exitPressed), for: .touchUpInside)
-        exitButton.backgroundColor = .gray
-        exitButton.layer.cornerRadius = 10
-        exitButton.translatesAutoresizingMaskIntoConstraints = false
-        return exitButton
-    }()
-    
-    @objc func exitPressed() {
-        profileView.avatarImage.alpha = 1
-        UIView.animate(withDuration: 0.5) {
-            self.avatarView.alpha = 0
-            
-        }
-    }
-    
-    private lazy var descriptionAvatar: UILabel = {
-        let descriptionAvatar = UILabel()
-        descriptionAvatar.font = .systemFont(ofSize: 12, weight: .light)
-        descriptionAvatar.numberOfLines = 100
-        descriptionAvatar.textAlignment = .center
-        descriptionAvatar.text = "Bill Gates, in full William Henry Gates III, (born October 28, 1955, Seattle, Washington, U.S.), American computer programmer and entrepreneur who cofounded Microsoft Corporation, the world’s largest personal-computer software company.Gates wrote his first software program at the age of 13. In high school he helped form a group of programmers who computerized their school’s payroll system and founded Traf-O-Data, a company that sold traffic-counting systems to local governments. In 1975 Gates, then a sophomore at Harvard University, joined his hometown friend Paul G."
-        descriptionAvatar.translatesAutoresizingMaskIntoConstraints = false
-        return descriptionAvatar
+    private lazy var tabelView: UITableView = {
+        let tabelView = UITableView(frame: .zero, style: .grouped)
+        tabelView.delegate = self
+        tabelView.dataSource = self
+        tabelView.layer.cornerRadius = 10
+        tabelView.register(PostHeaderViewCell.self, forCellReuseIdentifier: Cells.postCell)
+        tabelView.register(PhotosTableViewCell.self, forCellReuseIdentifier: Cells.photoCell)
+        tabelView.register(MyLootsViewCell.self, forCellReuseIdentifier: Cells.labelCell)
+        tabelView.register(LikesViewCell.self, forCellReuseIdentifier: Cells.likesCell)
+        tabelView.translatesAutoresizingMaskIntoConstraints = false
+        return tabelView
     }()
     
     @objc func onTapScreen() {
         
         UIView.animate(withDuration: 0.5) {
-            self.profileView.avatarImage.alpha = 0
-            self.avatarView.alpha = 0.9
+            self.avatarView.alpha = 0.95
         }
-        
-        avatarView.addSubview(exitButton)
-        avatarView.addSubview(avatarImage)
-        avatarView.addSubview(descriptionAvatar)
-        
-        
-        var constraints = [NSLayoutConstraint] ()
-        
-        constraints.append(exitButton.topAnchor.constraint(equalTo: avatarView.topAnchor, constant: 80))
-        constraints.append(exitButton.trailingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: -30))
-        constraints.append(exitButton.heightAnchor.constraint(equalToConstant: 30))
-        constraints.append(exitButton.widthAnchor.constraint(equalToConstant: 30))
-        
-        constraints.append(avatarImage.topAnchor.constraint(equalTo: exitButton.bottomAnchor, constant: 30))
-        constraints.append(avatarImage.leadingAnchor.constraint(lessThanOrEqualTo: avatarView.leadingAnchor, constant: 150))
-        constraints.append(avatarImage.trailingAnchor.constraint(greaterThanOrEqualTo: avatarView.trailingAnchor, constant: -150))
-        constraints.append(avatarImage.heightAnchor.constraint(lessThanOrEqualToConstant: 350))
-        constraints.append(avatarImage.centerXAnchor.constraint(equalTo: avatarView.centerXAnchor))
-        
-        constraints.append(descriptionAvatar.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 20))
-        constraints.append(descriptionAvatar.bottomAnchor.constraint(lessThanOrEqualTo: avatarView.safeAreaLayoutGuide.bottomAnchor, constant: -20))
-        constraints.append(descriptionAvatar.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor, constant: 30))
-        constraints.append(descriptionAvatar.trailingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: -30))
-        
-        NSLayoutConstraint.activate(constraints)
     }
     
     var posts: [PostView] = []
@@ -116,21 +57,9 @@ class ProfileViewController: UIViewController {
      
     private func setupConstraints() {
         
-        profileView.translatesAutoresizingMaskIntoConstraints = false
-        postView.translatesAutoresizingMaskIntoConstraints = false
-        
         posts = fetchData()
-        let tabelView = UITableView()
-        tabelView.delegate = self
-        tabelView.dataSource = self
-        tabelView.layer.cornerRadius = 10
-        tabelView.sectionHeaderHeight = 250
-        tabelView.estimatedRowHeight = 220
-        tabelView.translatesAutoresizingMaskIntoConstraints = false
-        tabelView.register(PostHeaderViewCell.self, forCellReuseIdentifier: Cells.postCell)
-        tabelView.register(PhotosTableViewCell.self, forCellReuseIdentifier: Cells.photoCell)
-        tabelView.register(MyLootsViewCell.self, forCellReuseIdentifier: Cells.labelCell)
-        tabelView.register(LikesViewCell.self, forCellReuseIdentifier: Cells.likesCell)
+        avatarView.translatesAutoresizingMaskIntoConstraints = false
+        postView.translatesAutoresizingMaskIntoConstraints = false
 
         view.addSubview(tabelView)
         view.addSubview(avatarView)
